592aee06d06413360ec277a8f4d453ee0874f7c5
yari-dewalt <yari.dewalt@gmail.com>
2024-02-04 21:13:33 -0800

    Create Message model

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const { DateTime } = require("luxon");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  text: { type: String, required: true, minLength: 1, maxLength: 200 },
+  timestamp: { type: Date, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true }
+}, { collection: "messages" });
+
+MessageSchema.virtual("formatted_time").get(function() {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString({ hour: "numeric", minute: "2-digit" });
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
