0c475bf2d587004ea384c199aebc3a49e55fc948
yari-dewalt <yari.dewalt@gmail.com>
2024-02-05 17:28:01 -0800

    Add member attribute (boolean)

--- models/user.js
+++ models/user.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true, minLength: 1, maxLength: 20 },
   username: { type: String, required: true, minLength: 1, maxLength: 50 },
-  password: { type: String, required: true, minLength: 1, maxLength: 30 },
-  messages: { type: [Schema.Types.ObjectId], ref: "Message" }
+  password: { type: String, required: true, minLength: 1, maxLength: 60 },
+  messages: { type: [Schema.Types.ObjectId], ref: "Message" },
+  member: { type: Boolean, default: false }
 }, { collection: "users" });
 
 module.exports = mongoose.model("User", UserSchema);
