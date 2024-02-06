55cb885dfa28b57030193f487817fd99c2896c32
yari-dewalt <yari.dewalt@gmail.com>
2024-02-05 17:28:30 -0800

    Update router

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const user_controller = require("../controllers/userController");
 const message_controller = require("../controllers/messageController");
 
 router.get("/", message_controller.index);
 
-router.get("/create", message_controller.message_create_get);
+/// USERS ///
+router.get("/signup", user_controller.user_create_get);
 
-router.post("/create", message_controller.message_create_post);
+router.post("/signup", user_controller.user_create_post);
+
+router.get("/login", user_controller.user_login_get);
+
+router.post("/login", user_controller.user_login_post);
+
+router.post("/make-member", user_controller.user_make_member);
+
+/// MESSAGES ///
+router.get("/newmessage", message_controller.message_create_get);
+
+router.post("/newmessage", message_controller.message_create_post);
 
 module.exports = router;
