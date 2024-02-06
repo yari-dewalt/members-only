9705315a9047282b5d1ff06246fe4a7d1fec7447
yari-dewalt <yari.dewalt@gmail.com>
2024-02-05 17:26:17 -0800

    Setup passport and session for logging in

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
+const User = require("./models/user");
 require("dotenv").config();
 
 const router = require("./routes/router");
@@ -19,13 +21,52 @@ async function main() {
   await mongoose.connect(mongoDB);
   console.log("Connected to MongoDB!");
 }
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 app.use("/", router);
 
 app.listen(3000, () => console.log("App listening on port 3000"));
